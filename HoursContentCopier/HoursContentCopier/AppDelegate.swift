430f3536e8f8e0af34623e86b72d050d4028fb71
Namho Kim <nhkimnamo@gmail.com>
2019-09-24 16:55:55 +0900

    init commit

---
+++ HoursContentCopier/HoursContentCopier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HoursContentCopier
+//
+//  Created by namho.kim on 24/09/2019.
+//  Copyright © 2019 namo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HoursContentCopier/HoursContentCopier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HoursContentCopier
+//
+//  Created by namho.kim on 24/09/2019.
+//  Copyright © 2019 namo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
