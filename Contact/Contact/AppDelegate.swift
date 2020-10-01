0fed50d7bf54f05e239dcb79142a221c8b502bfd
Namho Kim <nhkimnamo@gmail.com>
2020-10-01 18:07:38 +0900

    skeleton by xcode template with Storyboard interface

---
+++ Contact/Contact/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Contact
+//
+//  Created by 김남호 on 20-10-1.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Contact/Contact/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Contact
+//
+//  Created by 김남호 on 20-10-1.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
